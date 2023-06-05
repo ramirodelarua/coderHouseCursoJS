31dde17c5f4d6fc17a673d56df8f082ef8f20db4
unknown <ramirodelarua@hotmail.com>
2023-06-05 03:06:11 -0300

    Terminada la Preentrega 01 pero con error de ciclo for

--- Preentrega 01/index.js
+++ Preentrega 01/index.js
@@ -1,17 +1,42 @@
-let expensaOrdinaria = prompt("¿Cuánto pagás de expensas ordinarias? Ingresar sólo números enteros");
-let expensaExtraordinaria = prompt("¿Cuánto pagás de expensas extraordinarias? Ingresar sólo números enteros");
-let deudaAnterior= prompt("¿Cuánto debés del mes anterior? Ingresar sólo números enteros");
-let interesesPunitorios = prompt("¿Cuánto te cobraron de intereses punitorios? Ingresar sólo números enteros");
+let expensaOrdinaria = parseInt(prompt("¿Cuánto pagás de expensas ordinarias? Ingresar sólo números enteros"),);
+let expensaExtraordinaria = parseInt(prompt("¿Cuánto pagás de expensas extraordinarias? Ingresar sólo números enteros"));
+let deudaAnterior= parseInt(prompt("¿Cuánto debés del mes anterior? Ingresar sólo números enteros"));
+let interesesPunitorios = parseInt(prompt("¿Cuánto te cobraron de intereses punitorios? Ingresar sólo números enteros"));
 let sumaExpensasOrdinariasTotales = expensaOrdinaria + deudaAnterior + interesesPunitorios;
-let inquilinoOrPropietario = prompt("Si sos propietario, ingresá 1 en el casillero, si sos inquilino, ingresá 2")
+let inquilinoOrPropietario = parseInt(prompt("Si sos propietario, ingresá 1 en el casillero, si sos inquilino, ingresá 2"));
 let sumaTotal = sumaExpensasOrdinariasTotales + expensaExtraordinaria;
 function avisoDePago(){
-	if(inquilinoOrPropietario == 1){
+	if(inquilinoOrPropietario === 1){
 		alert("Debés un total de " + sumaTotal + " pesos. Recordá que tenés tiempo hasta el 10 de cada mes para pagarlo");
-	}else if(inquilinoOrPropietario == 2){
+	}else if(inquilinoOrPropietario === 2){
 		alert("Debés un total de " + sumaExpensasOrdinariasTotales + " pesos. Recordá que tenés tiempo hasta el 10 de cada mes para pagarlo");
 	}else{
 		alert("Usted ha ingresado una opción incorrecta. Por favor, pulse F5 y vuelva a consultar sus expensas")
 	}
+} avisoDePago();
+let preguntaDeuda = parseInt(prompt("¿Cuántos meses de expensas debés?"));
+function alertaDeuda(){
+	for(i = 0 , i <= 3 , i++){
+		if(preguntaDeuda > i){
+			alert("La unidad registra una deuda de más de tres (3) meses. En caso de no cancelarla en la inmediatez se intimará fehacientemente conforme a la ley vigente.");
+		}else if(preguntaDeuda > 0){
+			alert("La unidad no posee deuda significativa. Aún así les pedimos mantener al día las expensas para que el consorcio pueda cumplir con sus obligaciones.");
+		}else if(preguntaDeuda < 4){
+			alert("La unidad no posee deuda significativa. Aún así les pedimos mantener al día las expensas para que el consorcio pueda cumplir con sus obligaciones.");
+		}else{
+		alert("La unidad no presenta deudas.");
+	}}
 }
-avisoDePago();
+
+alertaDeuda();
+
+// *SEGUNDO MODELO DE CONSULTOR DE EXPENSAS ONLINE CON OBJETOS*
+
+/* let expensasUFX = {
+	expensaOrdinaria = 16175,
+	expensaExtraordinaria = 11221,
+	expensasAysa = 1391,
+	deudaAnterior = 25698,
+	interesesPunitorios = 770,
+}
+*/
