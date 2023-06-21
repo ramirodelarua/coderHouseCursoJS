90cd88fe456e2410af64a66ab81320a8b76df13f
unknown <ramirodelarua@hotmail.com>
2023-06-21 19:59:56 -0300

    Sigo modificando la información

--- Preentrega 02/index.js
+++ Preentrega 02/index.js
@@ -15,36 +15,27 @@ function Propietario(nombre, apellido, unidadFuncional, porcentualA, porcentualB
     this.porcentualD = porcentualD;
 }
 
-function Proveedor(consorcio, razonSocial, cuit, numeroDeFactura, concepto, importe, cuotas){
-    this.consorcio = consorcio;
-    this.razonSocial = razonSocial;
-    this.cuit = cuit;
-    this.numeroDeFactura = numeroDeFactura;
-    this.concepto = concepto;
-    this.importe = importe;
-    this.cuotas = cuotas;
-}
-// CONSORCIOS //
-const consorcioFreire2281 = new ConsorcioDePropietarios("Cap. Gral. Ramón Freire", 2281, 1428, 17);
-const consorcioChenaut1916 = new ConsorcioDePropietarios("Av. Gral. Indalecio Chenaut", 1916, 1426, 12);
+// CONSORCIOS: Calle, altura, CP, cant. de UF//
+const consorcioCuchacucha2281 = new ConsorcioDePropietarios("Cucha Cucha", 2281, 1428, 17);
+const consorcioLibertador1916 = new ConsorcioDePropietarios("Av. del Libertador 1916", 1916, 1426, 12);
 // PROPIETARIOS //
-const propietarioFreire2281UF01 = new Propietario("SDGSD", "FBJ6T", 1, 6.35, 0, 0, 0);
-const propietarioFreire2281UF02 = new Propietario("HRJJS", "SJ6SQ", 2, 6.43, 0, 0, 0);
-const propietarioFreire2281UF03 = new Propietario("POIYUGN", "DTJTRJN", 3, 6.36, 0, 0, 0);
-const propietarioFreire2281UF04 = new Propietario("SSHW", "ERJEFDV", 4, 6.47, 0, 0, 0);
-const propietarioFreire2281UF05 = new Propietario("SFHHW", "WRHJ5", 5, , 0, 0, 0);
-const propietarioFreire2281UF06 = new Propietario("ERJETBV", "SDVDE", 6, 5.65, 0, 0, 0);
-const propietarioFreire2281UF07 = new Propietario("SRJTJHV", "SWDVCE", 7, 5.65, 0, 0, 0);
-const propietarioFreire2281UF08 = new Propietario("FRHTEJHV", "SFHETJH", 8, 6.28, 0, 0, 0);
-const propietarioFreire2281UF09 = new Propietario("SDFSETJH", "SFHETHV", 9, 6.28, 0, 0, 0);
-const propietarioFreire2281UF10 = new Propietario("SDCQC", "CWWXXZ", 10, 5.46, 0, 0, 0);
-const propietarioFreire2281UF11 = new Propietario("WEGRWGB", "SDVRB", 11, 5.46, 0, 0, 0);
-const propietarioFreire2281UF12 = new Propietario("NMNYMR", "YMJIKY", 12, 6.09, 0, 0, 0);
-const propietarioFreire2281UF13 = new Propietario("TNTRVCW", "ERERTT", 13, 6.09, 0, 0, 0);
-const propietarioFreire2281UF14 = new Propietario("VFNNB", "NVBVS", 14, 5.27, 0, 0, 0);
-const propietarioFreire2281UF15 = new Propietario("DSBVRENH", "BFBEREW", 15, 5.27, 0, 0, 0);
-const propietarioFreire2281UF16 = new Propietario("ASGRG", "SFBTNJ", 16, 5.21, 0, 0, 0);
-const propietarioFreire2281UF17 = new Propietario("SBRBBS", "SFRBRH", 17, 5.21, 0, 0, 0);
+const propietarioCuchacucha2281UF01 = new Propietario("VALERIA", "SIMON", 1, 6.35, 0, 0, 0);
+const propietarioCuchacucha2281UF02 = new Propietario("BRAIAN", "GOMEZ", 2, 6.43, 0, 0, 0);
+const propietarioCuchacucha2281UF03 = new Propietario("JUN LYN", "DTJTRJN", 3, 6.36, 0, 0, 0);
+const propietarioCuchacucha2281UF04 = new Propietario("DARIO", "ASSIF", 4, 6.47, 0, 0, 0);
+const propietarioCuchacucha2281UF05 = new Propietario("MIGUEL", "GRECCO", 5, , 0, 0, 0);
+const propietarioCuchacucha2281UF06 = new Propietario("TOMÁS", "DE VICTORIA", 6, 5.65, 0, 0, 0);
+const propietarioCuchacucha2281UF07 = new Propietario("ANA", "NODUERME", 7, 5.65, 0, 0, 0);
+const propietarioCuchacucha2281UF08 = new Propietario("ANTONIO", "RIOS", 8, 6.28, 0, 0, 0);
+const propietarioCuchacucha2281UF09 = new Propietario("JOSÉ", "PRIETO", 9, 6.28, 0, 0, 0);
+const propietarioCuchacucha2281UF10 = new Propietario("HELGA", "CLARIS", 10, 5.46, 0, 0, 0);
+const propietarioCuchacucha2281UF11 = new Propietario("GASTÓN", "RANCIO", 11, 5.46, 0, 0, 0);
+const propietarioCuchacucha2281UF12 = new Propietario("FRANCISCA", "CASTRO", 12, 6.09, 0, 0, 0);
+const propietarioCuchacucha2281UF13 = new Propietario("ELSA", "ETEL", 13, 6.09, 0, 0, 0);
+const propietarioCuchacucha2281UF14 = new Propietario("RAÚL", "MICCON", 14, 5.27, 0, 0, 0);
+const propietarioCuchacucha2281UF15 = new Propietario("TOMÁS", "FRESCO", 15, 5.27, 0, 0, 0);
+const propietarioCuchacucha2281UF16 = new Propietario("CECILIA", "DABAS", 16, 5.21, 0, 0, 0);
+const propietarioCuchacucha2281UF17 = new Propietario("MARCELO", "ELTON", 17, 5.21, 0, 0, 0);
 
 const propietarioChenaut1916UF01 = new Propietario("PEPITO", "PEREZ", 1, 7.25, 7.58, 0, 0);
 const propietarioChenaut1916UF02 = new Propietario("AGUSTINA", "GOMEZ", 2, 7.92, 8.05, 0, 0);
@@ -59,6 +50,18 @@ const propietarioChenaut1916UF10 = new Propietario("ERNESTINA", "GALLARDO", 10, 
 const propietarioChenaut1916UF11 = new Propietario("SAUL", "MENDEZ", 11, 9.04, 8.82, 0, 0);
 const propietarioChenaut1916UF12 = new Propietario("NEW HORIZON & ASOCIADOS", "", 12, 8.09, 8.17, 0, 0);
 
-// FACTURA DE PROVEEDORES //
+let consorcioCuchacucha2281gastoExpA = 100000;
+let consorcioCuchacucha2281gastoExpB = 50000;
+let consorcioCuchacucha2281gastoExpC = 0;
+let consorcioCuchacucha2281gastoExpD = 0;
+let consorcioCuchacucha2281gastosTotales = gastoExpA + gastoExpB + gastoExpC + gastoExpD;
 
+let consorcioLibertador1916gastoExpA = 300000;
+let consorcioLibertador1916gastoExpB = 15000;
+let consorcioLibertador1916gastoExpC = 60456;
+let consorcioLibertador1916gastoExpD = 0;
+let consorcioLibertador1916gastosTotales = gastoExpA + gastoExpB + gastoExpC + gastoExpD;
 
+function consorcioLibertador1916SumarExpensasTotales(){
+    for(i=0 ; i <  )
+}
